4cc37ad873525a026c8ffc8dc5dcdeaabfaa7df7
MariusDevelops <brazasmarius@gmail.com>
2022-02-01 23:12:51 +0100

    Toggle show / hide element

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ function darkMode() {
   const element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+// show/hide
+function showHide() {
+  var x = document.getElementById("showHide");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+//  if (x.style.display === "none") {
+//    x.style.display = "block";
+//  } else {
+//    x.style.display = "none";
+//  }
