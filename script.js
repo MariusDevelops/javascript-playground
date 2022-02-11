937d429fa0879cb7ddefb76b93c18330b226cae1
MariusDevelops <brazasmarius@gmail.com>
2022-02-11 22:27:25 +0100

    feat: my first animation

--- script.js
+++ script.js
@@ -58,3 +58,21 @@ function toggleIcon(x) {
   x.classList.toggle("fa-thumbs-down");
 }
 ////////// TOGGLE END ////////////
+
+//////// MY FIRST ANIMATION ////////
+function myMove() {
+  let id = null;
+  const elem = document.getElementById("animate");
+  let pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + "px";
+      elem.style.left = pos + "px";
+    }
+  }
+}
